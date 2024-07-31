687eb6364a05874cd208e1d74e8e52f5d66efb24
RomanRavlyk <Roman.Ravlyk.RI.2023@lpnu.ua>
2024-07-31 18:47:03 +0300

    feat: Added PostgresQL to project

--- Shop/settings.py
+++ Shop/settings.py
@@ -79,26 +79,26 @@ WSGI_APPLICATION = 'Shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'Sneakers shop',
-#         'USER': 'admin',
-#         'PASSWORD': 'admin',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     },
-#     "OPTIONS": {
-#         "init_command": "SET default_storage_engine=INNODB",
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Sneakers Shop',
+        'USER': 'DataBase manager',
+        'PASSWORD': 'postgresmanager',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
+    "OPTIONS": {
+        "init_command": "SET default_storage_engine=INNODB",
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
